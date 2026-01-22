be7e5dbae0e4f1d8715d44b091cf63946d70d0c6
yashpal8078 <parmaryashpal9460@gmail.com>
2026-01-23 00:19:01 +0530

    Added the tabulation method and understand it , how this solve the problem of the stack overflow condition in some of the standard question

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,9 +1,14 @@
 class Fibonacci{
     public static void main(String[] args) {
         int n = 10; 
+        //using memorization
         System.out.println("Fibonacci number at position " + n + " is: " + fibonacci(n, new int[n + 1]));
+        //using tabulation
+        System.out.println("Fibonacci number at position " + n + " is: " + fibonacciTabulation(n));
     }
 
+
+    //memorization approach
     public static int fibonacci(int n , int[] memo) {
         if (n <= 0) {
             return 0;
@@ -15,5 +20,21 @@ class Fibonacci{
         }
 
         return memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    }
+
+    //tabulation approach
+    public static int fibonacciTabulation(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
